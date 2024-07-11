379c384021e151fb2a940e087d72652463402a7e
zodern <zodern@users.noreply.github.com>
2024-07-11 09:56:50 -0500

    v1.7.0-beta.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "CPU Profiler for Meteor (used with Monti APM)",
-  "version": "1.7.0-beta.1",
+  "version": "1.7.0-beta.2",
   "git": "https://github.com/monti-apm/meteor-profiler.git",
   "name": "montiapm:profiler"
 });
